4ae0ed7c515bdd503f684326979d10fa05ed4540
LarfaouiYounes7 <youneslarfaoui7@gmail.com>
2023-03-06 11:57:42 +0100

    dashboard added successfully

--- laravel-app/resources/views/home.blade.php
+++ laravel-app/resources/views/home.blade.php
@@ -1,32 +1,5 @@
-@extends('layouts.app')
+@extends('layouts.Dashboard')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header text-center">
-                        <span class ="text-success">
-                        SCHOOL TABLE MANAGEMENT
-                        </span>
-                        <span class ="text-danger">
-                         LANGUAGE E-LEARNING
-                        </span>
-                    <!-- {{ __('Dashboard') }} -->
-                </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <!-- {{ __('You are logged in!') }} -->
-                </div>
-            </div>
-            
-        </div>
-    </div>
-</div>
 @endsection
